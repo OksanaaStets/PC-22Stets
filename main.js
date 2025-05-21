d5137ef680bed932ca86673baa341268535c315b
OksanaaStets <oksanastets12@gmaol.com>
2025-05-21 23:16:10 +0300

    Редагування html

---
+++ main.js
@@ -0,0 +1,13 @@
+// Код для реєстрації Service Worker
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(registration => {
+        console.log('Service Worker зареєстровано успішно:', registration.scope);
+      })
+      .catch(error => {
+        console.log('Реєстрація Service Worker не вдалась:', error);
+      });
+  });
+}
