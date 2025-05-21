c0fbd4f0293bcc3c54cb6e32cda9e6f992ad4a86
OksanaaStets <oksanastets12@gmaol.com>
2025-05-22 01:00:44 +0300

    Редагування html

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register('/PC-22Stets/sw.js')
       .then(registration => {
         console.log('Service Worker зареєстровано успішно:', registration.scope);
       })
